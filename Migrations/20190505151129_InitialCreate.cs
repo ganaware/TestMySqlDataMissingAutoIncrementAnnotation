e4d2db1455207e537af968095e13f0b6a079bbe2
Nayuta Taga <nayuta@ganaware.jp>
2019-05-06 00:22:56 +0900

    first commit

---
+++ Migrations/20190505151129_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestMySqlDataMissingAutoIncrementAnnotation.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bars",
+                columns: table => new
+                {
+                    Baz_Data = table.Column<int>(nullable: false),
+                    BarId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bars", x => x.BarId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Foos",
+                columns: table => new
+                {
+                    FooId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Baz_Data = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Foos", x => x.FooId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bars");
+
+            migrationBuilder.DropTable(
+                name: "Foos");
+        }
+    }
+}
---
+++ MyContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestMySqlDataMissingAutoIncrementAnnotation {
+    public class Foo {
+        public int FooId { get; set; }
+        public Baz Baz { get; set; }
+    }
+
+    public class Bar {
+        public Baz Baz { get; set; }
+        public int BarId { get; set; }
+    }
+
+    public class Baz {
+        public int Data { get; set; }
+    }
+
+    public class MyContext : DbContext {
+        public DbSet<Foo> Foos { get; set; }
+        public DbSet<Bar> Bars { get; set; }
+
+        public MyContext() { }
+
+        public MyContext(DbContextOptions<MyContext> options)
+            : base(options) {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            if (!optionsBuilder.IsConfigured) {
+                optionsBuilder.UseMySQL(
+                    "Host=localhost",
+                    o => {
+                        o.MigrationsAssembly("TestMySqlDataMissingAutoIncrementAnnotation");
+                    });
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            {
+                var etb = modelBuilder.Entity<Foo>();
+                etb.Property(e => e.FooId);
+                etb.HasKey(e => e.FooId);
+                etb.OwnsOne(e => e.Baz, rob => rob.Property(e => e.Data));
+            }
+            {
+                var etb = modelBuilder.Entity<Bar>();
+                etb.Property(e => e.BarId);
+                etb.HasKey(e => e.BarId);
+                etb.OwnsOne(e => e.Baz, rob => rob.Property(e => e.Data));
+            }
+        }
+    }
+}
